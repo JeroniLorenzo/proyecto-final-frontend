60548a9378afd6012928c52aea29ac4c90861cb4
JeroniLorenzo <jeronilorenzosolano@gmail.com>
2023-03-19 16:26:19 +0100

    añadida expresion regular para direcciones de calles con numero

--- src/services/utiles.js
+++ src/services/utiles.js
@@ -5,7 +5,7 @@ export const Decoder = (token) => {
     const decodedToken = decodeToken(token);
 
     return decodedToken;
-    
+
 };
 
 export const errorCheck = (name, value) => {
@@ -16,9 +16,9 @@ export const errorCheck = (name, value) => {
         case 'surname':
 
 
-            if (! /[a-z]/gi.test(value) ) {
+            if (! /[a-z]/gi.test(value)) {
                 return ("Formato de texto inválido");
-                
+
             } else {
                 return '';
             };
@@ -26,26 +26,26 @@ export const errorCheck = (name, value) => {
         case 'email':
 
             if (! /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value)) {
-                return "Email en formato inválido";
+                return "Formato de email inválido";
             } else {
                 return '';
             }
 
         case 'phone':
 
-            if (! /(?=.*?[0-9])/.test(value) ) {
+            if (! /(?=.*?[0-9])/.test(value)) {
                 return "Teléfono incorrecto";
             } else {
                 return "";
             }
 
-         case 'password':
+        case 'password':
 
-             if (value.length < 8) {
-                 return "Write 8 characters at least"
-             } else {
+            if (value.length < 8) {
+                return "La contraseña debe ser mínimo de 8 carácteres y contener mínimo un número"
+            } else {
 
-                 //Checking the password format....
+                //Checking the password format....
 
                 if (! /[\d()+-]/g.test(value)) {
                     return "Password en formato inválido";
@@ -53,6 +53,15 @@ export const errorCheck = (name, value) => {
                     return "";
                 }
             }
+
+        case 'address':
+           if(! /^[a-zA-Zá-úÁ-Ú]+\s\d{1,4}(-\d{1,4})?\s?[a-zA-Zá-úÁ-Ú]*$/i.test(value)){
+            return 'Formato de dirección incorrecto';
+           } else{
+            return '';
+           }
+
+
 
         default:
             console.log("what are you sending to me????");
